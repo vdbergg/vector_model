be3110234ccb02d702baa8f817cca5cff7581af7
berg <vdbdgf@icomp.ufam.edu.br>
2018-10-20 13:06:21 -0400

    Altera MinHeap para receber payload com DocId e Score

--- wand/MinHeap.cpp
+++ wand/MinHeap.cpp
@@ -6,10 +6,15 @@
 #include<climits>
 using namespace std;
 
-void swap(int *x, int *y);
+struct HeapPayload {
+    int docId;
+    double score;
+};
+
+void swap(HeapPayload *x, HeapPayload *y);
 
 class MinHeap {
-    int *heap;
+    HeapPayload *heap;
     int capacity;
     int currentSize;
 public:
@@ -23,56 +28,61 @@ public:
 
     int right(int i) { return (2 * i + 2); }
 
-    int extractMin();
+    HeapPayload extractMin();
 
-    void decreaseKey(int i, int newValue);
+    void decreaseKey(int i, HeapPayload newValue);
 
-    int getMin() { return heap[0]; }
+    HeapPayload getMin() { return heap[0]; }
 
     void deleteKey(int i);
 
-    void insertKey(int key);
+    bool insertKey(HeapPayload key);
 };
 
 MinHeap::MinHeap(int capacity) {
     currentSize = 0;
     this->capacity = capacity;
-    heap = new int[capacity];
+    heap = new HeapPayload[capacity];
 }
 
-void MinHeap::insertKey(int key) {
-    if (currentSize == capacity) {
-        cout << " Could not insertKey\n";
-        return;
-    }
+bool MinHeap::insertKey(HeapPayload key) {
+    if (currentSize == capacity) return false;
 
     currentSize++;
     int i = currentSize - 1;
     heap[i] = key;
 
-    while (i != 0 && heap[parent(i)] > heap[i]) {
+    while (i != 0 && heap[parent(i)].score > heap[i].score) {
        swap(&heap[i], &heap[parent(i)]);
        i = parent(i);
     }
+
+    return true;
 }
 
-void MinHeap::decreaseKey(int i, int newValue) {
+void MinHeap::decreaseKey(int i, HeapPayload newValue) {
     heap[i] = newValue;
-    while (i != 0 && heap[parent(i)] > heap[i]) {
+    while (i != 0 && heap[parent(i)].score > heap[i].score) {
        swap(&heap[i], &heap[parent(i)]);
        i = parent(i);
     }
 }
 
-int MinHeap::extractMin() {
-    if (currentSize <= 0) return INT_MAX;
+HeapPayload MinHeap::extractMin() {
+    if (currentSize <= 0) {
+        HeapPayload heapPayload;
+        heapPayload.score = -1;
+        heapPayload.docId = -1;
+
+        return heapPayload;
+    };
 
     if (currentSize == 1) {
         currentSize--;
         return heap[0];
     }
 
-    int root = heap[0];
+    HeapPayload root = heap[0];
     heap[0] = heap[currentSize - 1];
     currentSize--;
     MinHeapify(0);
@@ -82,7 +92,10 @@ int MinHeap::extractMin() {
 
 
 void MinHeap::deleteKey(int i) {
-    decreaseKey(i, INT_MIN);
+    HeapPayload heapPayload;
+    heapPayload.docId = heap[i].docId;
+    heapPayload.score = INT_MIN;
+    decreaseKey(i, heapPayload);
     extractMin();
 }
 
@@ -91,8 +104,8 @@ void MinHeap::MinHeapify(int i) {
     int r = right(i);
     int smallest = i;
 
-    if (l < currentSize && heap[l] < heap[i]) smallest = l;
-    if (r < currentSize && heap[r] < heap[smallest]) smallest = r;
+    if (l < currentSize && heap[l].score < heap[i].score) smallest = l;
+    if (r < currentSize && heap[r].score < heap[smallest].score) smallest = r;
 
     if (smallest != i) {
         swap(&heap[i], &heap[smallest]);
@@ -100,8 +113,8 @@ void MinHeap::MinHeapify(int i) {
     }
 }
 
-void swap(int *x, int *y) {
-    int temp = *x;
+void swap(HeapPayload *x, HeapPayload *y) {
+    HeapPayload temp = *x;
     *x = *y;
     *y = temp;
 }
